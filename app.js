1a01b5c4e2155a8dbb21debd034925ab1b3e5fd4
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-01-21 15:59:20 +0000

    mensaje

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-let whotoblame = ["the dog", "my gramma", "the cat", "your mama"];
-let whattheydid = ["eat", "pissed", "crushed", "broke"];
-let whatdoyouwanttoavoid = [
-  "my homework",
-  "the project",
-  "the model",
-  "the book",
-];
-let when = [
-  "before the class",
-  "right in time",
-  "when I finished",
-  "during my breakfast",
-  "during sleep",
-  "michi",
-];
+let pronoun = ["the", "our", "a", "you"];
+let adj = ["yellow", "fat", "skinny", "red"];
+let noun = ["kitty", "cat", "pussycat", "kitten", "wildcat", "michi"];
+let extensions = [".com", ".net", ".us", ".io"];
 
 for (let i = 1; i < 9; i++) {
   let domain =
     i +
     " " +
-    whotoblame[Math.floor(Math.random() * whotoblame.length + 0)] +
-    " " +
-    whattheydid[Math.floor(Math.random() * whattheydid.length + 0)] +
-    " " +
-    whatdoyouwanttoavoid[
-      Math.floor(Math.random() * whatdoyouwanttoavoid.length + 0)
-    ] +
-    " " +
-    when[Math.floor(Math.random() * when.length + 0)];
+    pronoun[Math.floor(Math.random() * pronoun.length + 0)] +
+    adj[Math.floor(Math.random() * adj.length + 0)] +
+    noun[Math.floor(Math.random() * noun.length + 0)] +
+    extensions[Math.floor(Math.random() * extensions.length + 0)];
 
   console.log(domain);
 }
